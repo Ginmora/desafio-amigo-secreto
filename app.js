6d9b2dd19b7c3c8ad57093cfb3ca6019286bc190
Ginmora <gina.paola.amor@gmail.com>
2025-03-19 10:46:06 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("nombreAmigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+    input.value = ""; // Limpiar el campo de entrada
+    actualizarListaAmigos();
+}
+
+function actualizarListaAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = amigos.join(", ");
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+    document.getElementById("resultado").innerHTML = `Amigo sorteado: ${amigoSorteado}`;
+}
